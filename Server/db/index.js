00a57fd6adbbdba1d067d4b405ddfc0bf1a53b66
Justt-Mel <justinmelendezj@gmail.com>
2025-06-24 13:03:02 -0400

    created the tables

--- Server/db/index.js
+++ Server/db/index.js
@@ -1,1 +1,29 @@
 const client = require(".client")
+
+const seed = async () =>{
+    const SQL = `
+    DROP TABLE IF EXISTS review;
+    DROP TABLE IF EXISTS users;
+    DROP TABLE IF EXISTS tech;
+
+    CREATE TABLE users(
+    id UUID PRIMARY KEY,
+    username VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    is_admin DEFUALT false NOT NULL
+    );
+
+    CREATE TABLE tech(
+    id UUID PRIMARY KEY,
+    tech_name VARCHAR(100)UNIQUE,
+    );
+
+    CREATE TABLE review(
+    id UUID PRIMARY KEY
+    user_id UUID REFERENCES users(id) NOT NULL,
+    tech_id UUID REFERENCES tech(id) NOT NULL,
+    created_at TIMESTAMP DEFUALT now() NOT NULL,
+    updated_at TIMESTAP DEFUALT now(),
+    );
+    `
+}
