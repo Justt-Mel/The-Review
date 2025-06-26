4132d41057d2f8c7348fde4f478a36097ccc2759
Justt-Mel <justinmelendezj@gmail.com>
2025-06-26 08:25:12 -0400

    added the authentication route in the api index.js file

--- Server/api/index.js
+++ Server/api/index.js
@@ -4,5 +4,6 @@ const app = express.Router()
 app.use('/users', require('./users'))
 app.use('/tech', require('./tech'))
 app.use('/review', require('./review'))
+app.use('/authentication', require('./authentication'))
 
 module.exports = app
